5ed3f8e57d33701a456a44a93a6a1e19d05aa4ad
Allison Walters <72672602+allisonmwalters@users.noreply.github.com>
2024-09-11 15:03:32 -0400

    Update page.tsx

--- app/blog/about/page.tsx
+++ app/blog/about/page.tsx
@@ -2,8 +2,7 @@ export default function About() {
     return (<>
         <h1 className="text-blue-600">About</h1>
         <h2>Allison M Walters</h2>
-        <p>
-            Hi, I&apos;m a person who makes things and loves people. I am the webmaster of CAA | Advancing Art &amp; Design. I write science fiction, paint, draw, do photography, make videos, write poetry, and DJ in my spare time. I like creating ideas and objects. Sometimes I also help as a sci-fi copy editor or as an artist assistant. I have a black cat named Steven and an orange cat named Texas Pete.</p>
+        <p>Hi, I&apos;m a person who makes things and loves people. I am the webmaster of CAA | Advancing Art &amp; Design. I write science fiction, paint, draw, do photography, make videos, write poetry, and DJ in my spare time. I like creating ideas and objects. Sometimes I also help as a sci-fi copy editor or as an artist assistant. I have a black cat named Steven and an orange cat named Texas Pete.</p>
     </>
     );
 
