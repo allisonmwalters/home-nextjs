0485c809b28cb6d426ffb0b3bdc570cc636fb252
Allison Walters <72672602+allisonmwalters@users.noreply.github.com>
2024-09-11 14:46:41 -0400

    Delete app/learn directory

--- app/learn/nextjs/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Learn() {
-    return (<>
-        <h1>Learn Next.js</h1>
-        <p>Learn to use Next.js!</p>
-    </>
-    );
-
-}
--- app/learn/react/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Learn() {
-    return (<>
-        <h1>Learn React</h1>
-        <p>Learn to use React!</p>
-    </>
-    );
-
-}
