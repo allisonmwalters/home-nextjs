0e49473911806aaaeb7e78e3f9fe7223f639d928
allison <allison@allisonmwalters.com>
2024-09-27 14:55:42 +0000

    added ability to connect to database

--- app/blog/post/[id]/page.tsx
+++ app/blog/post/[id]/page.tsx
@@ -1,10 +1,13 @@
 import { posts } from '@/app/lib/placeholder-data.js';
 import Post from '@/app/ui/components/posts/Post';
+import { connectToDb } from '@/app/lib/data';
 
-export default function Page({ params }: { params: { id: string } }) {
+export default async function Page({ params }: { params: { id: string } }) {
     const post = posts.find((post) => post.id === params.id);
+    const client = await connectToDb();
     return (
         <>
+            {client && <p className='text-blue-500'>Connected to database</p>}
             <h1>Post</h1>
             {post && <Post {...post} />}
         </>)
---
+++ app/lib/data.tsx
@@ -0,0 +1,16 @@
+import { createClient } from "@vercel/postgres";
+
+export async function connectToDb() {
+    const client = createClient();
+    await client.connect();
+
+    try {
+        if (client) {
+            console.log("Connected to database");
+            return client;
+        }
+    }
+    catch (error) {
+        console.error("Error connecting to database", error);
+    }
+}
